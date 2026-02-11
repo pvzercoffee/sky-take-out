9eb7beb0c0bcfd1dbbe1186451f96074925530ba
pvzercoffee <pvzercoffee@gmail,com>
2026-02-12 02:21:26 +0800

    feat:新增菜品框架

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,13 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -12,6 +17,11 @@ public interface DishMapper {
      * @param categoryId
      * @return
      */
-    @Select("select count(*) from dish where category_id = #{categoryId}")
-    Integer countByCategoryId(Long categoryId);
+    @Select("select * from dish where category_id = #{categoryId}")
+    List<Dish> countByCategoryId(Long categoryId);
+
+    @Insert("insert into dish(name,category_id,price,image,description,status,create_time,update_time,create_user,update_user) " +
+            "values(#{name},#{categoryId},#{price},#{image},#{description},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(OperationType.INSERT)
+    void save(Dish dish);
 }
