96e97148bba3dfc279dcd630476a03857c2bfa15
pvzercoffee <pvzercoffee@gmail.com>
2026-02-12 01:37:00 +0800

    feat:公共字段代码填充

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -18,6 +20,7 @@ public interface CategoryMapper {
      */
     @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user) value" +
             "(#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(OperationType.INSERT)
     void insert(Category category);
 
     /**
@@ -39,6 +42,7 @@ public interface CategoryMapper {
      * 修改分类
      * @param category
      */
+    @AutoFill(OperationType.UPDATE)
     void update(Category category);
 
     /**
